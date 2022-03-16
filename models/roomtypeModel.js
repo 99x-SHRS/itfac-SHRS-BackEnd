cc02425f5431407f85e3fe8537955a0e552c8a25
Navod Dilshan <navoddilshan9@gmail.com>
2022-03-16 12:17:11 +0530

    json

--- models/roomtypeModel.js
+++ models/roomtypeModel.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
     attributes: {
-      type: DataTypes.STRING,
+      type: DataTypes.JSON,
     },
     description: {
       type: DataTypes.STRING,
